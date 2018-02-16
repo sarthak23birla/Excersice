8b6e9d736c53dc78e3944e0f48d871fac3208f6d
Sarthak Birla <sarthak23birla@gmail.com>
2018-02-16 15:52:30 +0530

    Iteration 4

--- task.rb
+++ task.rb
@@ -1,51 +1,44 @@
 def product_gst_slab_evaulation(product)
-	food_grains = ["rice","wheat"] # 0%
-	furniture = ["table","sofa","chair"] # 5%
-	electronics =["mobile","tv","tablets"] # 18%
-	cosmetics = ["beauty_cream","perfume"] # 28%
-	if(food_grains.include?(product))
-		return 0 #0% slab gst_price
-	elsif(furniture.include?(product))
-		return 0.05 #5 % slab gst_price
-	elsif(electronics.include?(product))
-		return 0.18 #18% slab gst_price
-	elsif(cosmetics.include?(product))		
-		return 0.28 #28% slab gst_price
-	end
+	food_grains = ["rice","wheat"];furniture = ["table","sofa","chair"];electronics =["mobile","tv","tablets"];cosmetics = ["beauty_cream","perfume"] # 28%
+	return 0    if food_grains.include?(product)
+	return 0.05 if furniture.include?(product)
+	return 0.18 if electronics.include?(product)
+	return 0.28 if cosmetics.include?(product)
 	return nil
 end
-def display_details(array_gst_price,overall_price)
-	p "The gst price are:#{array_gst_price.join(",")}"
-	p "Overall Price:#{overall_price}"
+
+def calculate_gst(overall_price,commodity_price,product_count,gst_percentage)
+	intial_price = commodity_price * product_count
+	price_including_gst = intial_price + intial_price * gst_percentage
+	overall_price += price_including_gst
+	puts "The GST per unit rate:#{intial_price * gst_percentage/product_count}"
+	return overall_price 
 end
 
-def calculate_gst_and_overall_price(array_commodity_data,array_commodity_price)
+def calculate_overall_price(commodity_datas,commodity_prices)
 	overall_price = 0
-	per_unit_gst_of_product = Array.new()
-	for _index in (0...array_commodity_price.length)
-		product_count,product = array_commodity_data[_index].split(" ")
-		product_count = product_count.to_i #Convert the String to integer
-		intial_price = array_commodity_price[_index] * product_count  #intial_prices
-		gst_percentage = product_gst_slab_evaulation(product)
+	for _index in (0...commodity_prices.length)
+		product_count,product = commodity_datas[_index].split(" ");gst_percentage = product_gst_slab_evaulation(product)
+		overall_price = calculate_gst(overall_price,commodity_prices[_index],product_count.to_i,gst_percentage)
 		if (gst_percentage.nil?)
 			p "Billing not Possible"
 			break
 		end
-		gst_price = intial_price * gst_percentage
-		price_including_gst = intial_price + gst_price
-		overall_price += price_including_gst
-		per_unit_gst_of_product.push(gst_price/product_count)
-		display_details(per_unit_gst_of_product,overall_price)
 	end
+	puts  "Overall Price:#{overall_price}"
 end
+
+def array_length_equal(commodity_datas,commodity_prices)
+	return  calculate_gst_and_overall_price(commodity_datas,commodity_prices) if(commodity_datas.length==commodity_prices.length)
+	return nil  
+end
+
 number_of_product = gets.chomp.to_i
 while number_of_product != 0
-	array_commodity_data = gets.chomp.split(",")   #String 1 to take input of commodity
-	array_commodity_price = gets.chomp.split(",").map{|e| e.to_i}  #String 2  to take input of prices
-	if (array_commodity_price.length == array_commodity_data.length)	
-		calculate_gst_and_overall_price(array_commodity_data,array_commodity_price)
-	else
-		p "Invalid Data Entry"
+	commodity_data = gets.chomp.split(",")   #String 1 to take input of commodity
+	commodity_price = gets.chomp.split(",").map{|e| e.to_i}  #String 2  to take input of prices
+	if (array_length_equal(commodity_data,commodity_price).nil?)
+		p " not valid "
 	end
 	number_of_product = number_of_product-1
 end
